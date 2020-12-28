3e8cac112a72301494601431e4f463a87239867b
Matt Haffner <mrhaffner13@gmail.com>
2020-12-28 17:28:23 -0600

    authors and books are now loaded with graphQL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
 import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const authorResult = useQuery(ALL_AUTHORS)
+  const bookResult = useQuery(ALL_BOOKS)
+
+  if (authorResult.loading || bookResult.loading)  {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -16,10 +24,12 @@ const App = () => {
       </div>
 
       <Authors
+        authors={authorResult.data.allAuthors}
         show={page === 'authors'}
       />
 
       <Books
+        books={bookResult.data.allBooks}
         show={page === 'books'}
       />
 
